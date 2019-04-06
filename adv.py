55c97ccba48382db2524b2ad9ecda6a39418ecd7
Nick1296 <mattianicolella@gmail.com>
2019-04-06 13:52:31 +0200

    added target model

--- adv.py
+++ adv.py
@@ -19,3 +19,10 @@ class AdversarialProgramming(tf.keras.Model):
         ps = [tf.tanh(self.W*self.M) for x in ps]
         prog = ps + inputs
         return prog
+
+def loadTargetNet(net):
+    if net == 'Inception_V3':
+        from tensorflow.keras.applications import InceptionV3
+        return (299, 299), InceptionV3()
+    else:
+        return 0, 0, None
